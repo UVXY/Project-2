31c5db8223ce5b98d47100a6ac90929a75a0f0cf
Lewis Gregerson-Spencer <lewisgregerson-spencer@Lewiss-MacBook.local>
2018-12-06 12:03:34 -0800

    fixed this shit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,24 @@
-var db = require("../models");
-
 module.exports = function(app) {
+    
   // Load index page
-  app.get("/", function(req, res) {
-    db.client.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/first", function(req, res) {
+      res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.client
-      .findOne({ where: { id: req.params.id } })
-      .then(function(dbExample) {
-        res.render("example", {
-          example: dbExample
-        });
-      });
+  app.get("/profile", function(req, res) {
+      res.render("layouts/profile");
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/signUp", function(req, res) {
+      res.render("layouts/signUp");
   });
+
+  app.get("/signUp2", function(req, res) {
+      res.render("layouts/signUp2");
+  });
+
+  app.get("/matches", function(req, res) {
+      res.render("layouts/matches");
+  });
+
 };
--- server.js
+++ server.js
@@ -1,47 +1,36 @@
-require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
-var db = require("./models");
+var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    "handlebars",
+    exphbs({
+      default: "index"
+    })
+  );
+  app.set("view engine", "handlebars");
+  app.use(express.static(__dirname + '/public'));
+  
+  //Sample GET request
+  app.get('/', function(req, res) {
+    
   });
-});
+  
+  //Sample POST request
+  app.post('/', function(req, res) {
+    
+  });
+  
+  // Routes
+  require("./routes/htmlRoutes")(app);
 
-module.exports = app;
+  app.listen(PORT, function() {
+      console.log("listening at PORT: " + PORT)
+  })
+
+  module.exports = app;
