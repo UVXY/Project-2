d2f2389cbb753d4303c6204d77a9ce1e139db988
ricky <ricky@rickys-MacBook-Air.local>
2018-12-08 00:29:13 -0800

    took off ignore modules

--- routes/activitiesApiRoutes.js
+++ routes/activitiesApiRoutes.js
@@ -138,28 +138,7 @@ module.exports = function(app) {
             // var secondNum = parseInt(req.params.secondNum);
             // var result;
             // Switch statement chooses operation based on the operation parameter.
-            // switch (operation) {
-            //   case "add":
-            //   case "+": // Bonus.  Example:  http://localhost:3002/+/6/4 --> 10
-            //     result = firstNum + secondNum;
-            //     break;
-            //   case "subtract":
-            //   case "-": // Bonus.  Example:  http://localhost:3002/-/6/4 --> 2
-            //     result = firstNum - secondNum;
-            //     break;
-            //   case "multiply":
-            //   case "*": // Bonus.  Example:  http://localhost:3002/*/3/4 --> 12
-            //     result = firstNum * secondNum;
-            //     break;
-            //   case "divide":
-            //   case "/": // Bonus.  This char has to be escaped in the url since slashes separate params
-            //     // Example:  http://localhost:3002/%2F/14/2 --> 7
-            //     result = firstNum / secondNum;
-            //     break;
-            //   default:
-            //     result =
-            //       "Sorry! The only valid operations are add, subtract, multiply, and divide.";
-            // }
+            // 
             // We return the result back to the user in the form of a string
             //   res.send(result.toString());
             // });
